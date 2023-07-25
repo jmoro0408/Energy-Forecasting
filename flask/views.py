079c873dc2cc573d9348c6bbfdfdff572cfe875f
jmoro0408 <james.moro.11@gmail.com>
2023-07-25 20:03:37 +0100

    add basic dataframe to /results/ route

--- flask/views.py
+++ flask/views.py
@@ -1,3 +1,8 @@
+import datetime
+from pathlib import Path
+
+import pandas as pd
+
 from flask import (Blueprint, jsonify, redirect, render_template, request,
                    url_for)
 
@@ -10,6 +15,31 @@ def home():
 @views.route("/profile/")
 def profile():
     return render_template("profile.html")
+
+def get_latest_forecast_date(saved_weather_forecast_dir:Path) -> str:
+    """returns the latest date present in the folder of saved weather forecasts
+
+    Args:
+        saved_weather_forecast_dir (Path): directory holding saved weather forecast parquet files
+
+    Returns:
+        str: latest forecast date
+    """
+    files = Path(saved_weather_forecast_dir).glob("*.parquet")
+    files = [p.stem for p in files]
+    files = [p[11:21] for p in files]
+    return sorted(files, key=lambda x: datetime.datetime.strptime(x, '%Y_%m_%d'),  reverse=True)[0]
+
+@views.route("/results/")
+def results():
+    PRED_SAVE_DIR = Path(Path.cwd().parent, "data.nosync", "outputs", "load_predictions")
+    latest_pred_date = get_latest_forecast_date(PRED_SAVE_DIR)
+    fname = f"prediction_{latest_pred_date}.parquet"
+    df = pd.read_parquet(Path(PRED_SAVE_DIR, fname))
+    return render_template("dataframe.html",
+                           column_names=df.columns.values,
+                           row_data=list(df.values.tolist()),
+                           zip=zip)
 
 
 @views.route("/json/")
